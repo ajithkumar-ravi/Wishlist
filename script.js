6183b790c3ea98cb76839ee7e138d4cdb2eef50f
Ajithkumar R <itsajith005@gmail.com>
2025-09-02 11:15:34 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let todoInput = document.querySelector(".input");
+let addTodoButton = document.querySelector(".button");
+let todo;
+
+addTodoButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    todo = todoInput.value
+    console.log("clicked");
+    
+})
