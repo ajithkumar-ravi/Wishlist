7c62116d8566431e975f6881fce7a5061152d399
ajithkumar-ravi <itsajith005@gmail.com>
2025-09-03 09:11:15 -0700

    create a random Number genarated funtion

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let todoInput = document.querySelector(".input");
 let addTodoButton = document.querySelector(".button");
+let showTodos = document.querySelector("todos-container")
 let todo;
+let todoList = [];
+
+// Create a function for get a unique id
+function uuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
 
 addTodoButton.addEventListener("click", (e) => {
     e.preventDefault();
-    todo = todoInput.value
-    console.log("clicked");
-    
+    todo = todoInput.value;
+    // console.log(todo);
+    if (todo.length > 0){
+        todoList.push({id: uuid(), todo, isCompleted : false})
+    }
 })
